623610699d46c7e01958b54789974f16c4c51024
madhurja16 <135540004+madhurja16@users.noreply.github.com>
2025-10-19 04:41:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ const galleryImages = [
     { src: "IMAGE/Gallery/14Aug.2023.png", caption: "Caption" },
     { src: "IMAGE/Gallery/29July_2023.png", caption: "Caption" },
     { src: "IMAGE/Gallery/07August_2023.png", caption: "Caption" },
-    { src: "IMAGE/Gallery/21_‎September_2023.png", caption: "Caption" },
+    { src: "IMAGE/Gallery/21_September_2023.png", caption: "Caption" },
     { src: "IMAGE/Gallery/10Oct_2023.png", caption: "Great vibes, amazing team • Lab dinner • 10/10/23" }
 ];
 
@@ -367,6 +367,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
 
 
 
